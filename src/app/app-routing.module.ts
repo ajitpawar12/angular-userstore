06a0f0b69908e024cffaf0d6532c224c367e61fd
Ajit <ajpawar2012@gmail.com>
2021-02-27 23:13:49 +0530

    Added initial code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'login',
+  component: LoginComponent
+}, 
+{
+  path: 'register',
+  component: RegistrationComponent
+},
+{
+  path: '',
+  component: LayoutComponent,
+  data:{allowedRoles: ['Admin','Dealer']},
+  children: [
+    { 
+      path: 'dashboard', component: LayoutComponent,
+      data: { 
+              allowedRoles: ['Admin','Dealer']
+            }
+    }]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { LayoutHeaderComponent } from './pages/layout-header/layout-header.component';
+import { LayoutMenuComponent } from './pages/layout-menu/layout-menu.component';
+import { LayoutFooterComponent } from './pages/layout-footer/layout-footer.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    LayoutHeaderComponent,
+    LayoutMenuComponent,
+    LayoutFooterComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
